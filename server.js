63936bf7133ed73c933c60357682a43cecc752e6
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-23 10:37:23 +0000

    further updates - added route files

--- server.js
+++ server.js
@@ -1,46 +1,52 @@
 const express = require("express")
 const app = express()
 const port = 3000
+const userRouter = require("./routes/user")
+const fruitRouter = require("./routes/fruit")
 
-// List of Users
-let users = [
-    {
-        name: "User 1",
-        age: 30
-    },
-    {
-        name: "User 2",
-        age: 45
-    },
-    {
-        name: "User 3",
-        age: 27
-    },
-    {
-        name: "User 4",
-        age: 22
-    }
-]
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/fruit", booksRouter)
 
-// List of Fruits
-let fruits = [
-    {
-        name: "Apple",
-        color: "Red"
-    },
-    {
-        name: "Banana",
-        color: "Yellow"
-    },
-    {
-        name: "Kiwi",
-        color: "Green"
-    },
-    {
-        name: "Grape",
-        color: "Purple"
-    },
-]
+// List of Users//
+// let users = [
+//     {
+//         name: "User 1",
+//         age: 30
+//     },
+//     {
+//         name: "User 2",
+//         age: 45
+//     },
+//     {
+//         name: "User 3",
+//         age: 27
+//     },
+//     {
+//         name: "User 4",
+//         age: 22
+//     }
+// ]
+
+// // List of Fruits
+// let fruits = [
+//     {
+//         name: "Apple",
+//         color: "Red"
+//     },
+//     {
+//         name: "Banana",
+//         color: "Yellow"
+//     },
+//     {
+//         name: "Kiwi",
+//         color: "Green"
+//     },
+//     {
+//         name: "Grape",
+//         color: "Purple"
+//     },
+// ]
 
 // Express Routes
 
