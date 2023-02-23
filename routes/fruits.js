339667fc66e417ca3db40cef0cdba4c6130240e0
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-23 15:43:56 +0000

    not everything complete/working

--- routes/fruits.js
+++ routes/fruits.js
@@ -24,16 +24,47 @@ let fruits = [
     },
 ]
 
-//FIND ALL musicians
+//get all
 
 fruit.get("/", async (req, res) => {
      let allFruit = await fruits;
      res.json(allFruit)
 })
 
+//get fruit by id
+
 fruit.get("/:id", (req, res)=>{
     res.json(fruits[req.params.id-1])
     })
+
+// post
+
+fruit.post("/", async (req, res) => {
+    try {
+      // const userObj = req.body;
+      const createFruit = await fruits.push(req.body);
+  
+      res.status(200).send({ msg: "Success", createFruit });
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  });
+
+  // PUT update - using where req.body - not confident this will work *****
+fruit.put("/", async (req, res) => {
+    try {
+      const updatedFruit = await fruits.update(req.body.update, {
+        where: req.body.where,
+      });
+      if (updatedFruit[0] > 0) {
+        res.status(200).send({ msg: "Success", updatedFruit});
+      } else {
+        throw new Error("No update made");
+      }
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  });
 
 
 module.exports = fruit;
--- routes/users.js
+++ routes/users.js
@@ -24,16 +24,68 @@ let users = [
 ]
 
 
-//.findAll()
+//get all
 
 user.get("/", async (req, res) => {
     let allUsers = await users;
     res.json(allUsers)
 })
 
+// get by id
 
 user.get("/:id", (req, res)=>{
     res.json(users[req.params.id-1])
     })
+
+
+    // post
+
+    user.post("/", async (req, res) => {
+        try {
+          // const userObj = req.body;
+          const createUser = await users.push(req.body);
+      
+          res.status(200).send({ msg: "Success", createUser });
+        } catch (error) {
+          res.status(500).send({ err: error.message });
+        }
+      });
+
+// put
+
+// PUT update - using where req.body - not confident this will work *****
+user.put("/", async (req, res) => {
+    try {
+      const updatedUser = await users.update(req.body.update, {
+        where: req.body.where,
+      });
+      if (updatedUser[0] > 0) {
+        res.status(200).send({ msg: "Success", updatedUser});
+      } else {
+        throw new Error("No update made");
+      }
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  });
+
+  // DELETE musician by ID using try and catch //not confident this willl work
+user.delete("/:id", async (req, res) => {
+    try {
+      // Cannot accept body
+      const deletedUser = users.destroy([req.params.id-1])
+  
+      if (deletedUser[0] > 0) {
+        res.status(200).send({ msg: "Success" });
+      } else {
+        throw new Error("Not deleted");
+      }
+    } catch (error) {
+      res.status(500).send({ err: error.message });
+    }
+  });
+
+
+
 
 module.exports = user;
