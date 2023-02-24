a2524b55c0b0b8c302257f1631f997a472b18fc3
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-24 12:04:52 +0000

    get, post, delete, put all added - not all tested

--- server.js
+++ server.js
@@ -9,45 +9,7 @@ app.use(express.urlencoded({extended:true}))
 app.use("/fruits", fruitRouter)
 app.use("/users", userRouter)
 
-// List of Users//
-// let users = [
-//     {
-//         name: "User 1",
-//         age: 30
-//     },
-//     {
-//         name: "User 2",
-//         age: 45
-//     },
-//     {
-//         name: "User 3",
-//         age: 27
-//     },
-//     {
-//         name: "User 4",
-//         age: 22
-//     }
-// ]
 
-// // List of Fruits
-// let fruits = [
-//     {
-//         name: "Apple",
-//         color: "Red"
-//     },
-//     {
-//         name: "Banana",
-//         color: "Yellow"
-//     },
-//     {
-//         name: "Kiwi",
-//         color: "Green"
-//     },
-//     {
-//         name: "Grape",
-//         color: "Purple"
-//     },
-// ]
 
 // Express Routes
 
